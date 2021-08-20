9bf4fbf9157dedc609b76c5d12c522f1f9979040
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2021-08-20 11:07:32 +0530

    merging of branch to master

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Welcome for merging");
             Console.WriteLine("Hello here");
         }
     }
